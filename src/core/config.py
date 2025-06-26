9530f0573b1474aaa93e6ab7a657e9950ebff649
MarcGroc <76005007+MarcGroc@users.noreply.github.com>
2025-06-26 10:14:26 +0200

    Fix/missing uv venv in actions pipelines (#3)
    * FIX/Missing uv venv in actions pipelines.
    
    * FIX/Incorrect path fix.

---
+++ src/core/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class DBSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="DB_")
+
+
+class SecuritySettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="SEC_")
+    SECRET: SecretStr = "secret"
+
+
+class Settings(BaseSettings):
+    """Loads variables from .env file"""
+
+    PROJECT_NAME: str = "Phising Detector"
+    DEBUG: bool = True
+
+    DB: DBSettings = DBSettings()
+    SECURITY: SecuritySettings = SecuritySettings()
+    model_config = SettingsConfigDict(env_file="./.env", env_file_encoding="utf-8")
+
+
+settings = Settings()
