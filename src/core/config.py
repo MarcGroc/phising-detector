9e8a0cd39191fde2dbb99f0c24a64efa8ea0e527
MarcGroc <marcingrochola1@gmail.com>
2025-06-26 09:30:31 +0200

    FIX/Incorrect path fix.

--- src/core/config.py
+++ src/core/config.py
@@ -8,17 +8,18 @@ class DBSettings(BaseSettings):
 
 class SecuritySettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="SEC_")
-    SECRET: SecretStr = 'secret'
+    SECRET: SecretStr = "secret"
 
 
 class Settings(BaseSettings):
     """Loads variables from .env file"""
+
     PROJECT_NAME: str = "Phising Detector"
     DEBUG: bool = True
 
     DB: DBSettings = DBSettings()
     SECURITY: SecuritySettings = SecuritySettings()
-    model_config = SettingsConfigDict(env_file='./.env', env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file="./.env", env_file_encoding="utf-8")
 
 
 settings = Settings()
